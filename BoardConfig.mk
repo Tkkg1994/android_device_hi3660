847b66241234eb595f60dd80cd351d92614ddd0d
Tkkg1994 <luca@grifodev.ch>
2017-09-27 00:12:30 +0200

    update the kernel to latest september base delete a few obsolent files name usb.rc to hi3660.usb.rc

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -25,7 +25,7 @@ ARCH_ARM_HAVE_TLS_REGISTER := true
 # Kernel
 BOARD_KERNEL_BASE := 0x00078000
 BOARD_KERNEL_PAGESIZE := 2048
-BOARD_KERNEL_CMDLINE := loglevel=4 initcall_debug=n page_tracker=on slub_min_objects=16 androidboot.selinux=permissive
+BOARD_KERNEL_CMDLINE := loglevel=4 initcall_debug=n page_tracker=on slub_min_objects=16 unmovable_isolate1=2:192M,3:224M,4:256M androidboot.selinux=permissive
 BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x00008000 --ramdisk_offset 0x07b88000 --second_offset 0x00e88000 --tags_offset 0x07988000
 TARGET_PREBUILT_KERNEL := device/huawei/hi3660/Image
 
